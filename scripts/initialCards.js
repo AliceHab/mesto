751725135ee86a77f0dca45b2c4993d319e37993
Maxim Novak <maksim.efremof@gmail.com>
2023-03-07 18:21:35 +0300

    refactor: вынос карточек по умолчанию в отдельный файл

---
+++ scripts/initialCards.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    name: 'Варшава',
+    link: 'https://images.unsplash.com/photo-1573157268772-519f309dc212?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+  },
+  {
+    name: 'Краков',
+    link: 'https://images.unsplash.com/photo-1573204376098-21156d4534fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+  },
+  {
+    name: 'Лодзь',
+    link: 'https://images.unsplash.com/photo-1677072340304-b002650d629b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+  },
+  {
+    name: 'Вроцлав',
+    link: 'https://images.unsplash.com/photo-1647079044618-ba91bd341112?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+  },
+  {
+    name: 'Познань',
+    link: 'https://images.unsplash.com/photo-1544657318-6198f65e31ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80',
+  },
+  {
+    name: 'Гданьск',
+    link: 'https://images.unsplash.com/photo-1664389169560-7b72b73ca651?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+  },
+];
+
+export default initialCards;
